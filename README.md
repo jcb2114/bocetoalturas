6c2cc20cf9514cdabc1140dedc515fe722300b48
jcb2114 <jcb2114@hotmail.es>
2021-04-08 17:06:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bocetoalturas-gr5jcs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bocetoalturas-gr5jcs)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    id: 1,
+    name: 'NIVEL AVANZADO TRABAJO SEGURO EN ALTURAS',
+    descripcion: '*REGIMEN SUBSIDIADO Y/O BENEFICIARIO*. - Ficha de Inscripción. - Fotocopia de la CC al 150%. - Estar registrado en la plataforma www.senasofiaplus.edu.co - Examen médico ocupacional (apto) para trabajo en alturas no mayor a un año (IPS avalada por la Secretaria de Salud Departamental), Soporte de afiliación estado activo EPS como subsidiado, si es Beneficiario presentar  Planilla de pago y afiliación del titular, Elementos de protección personal descritos en la ficha de inscripción.'
+  },
+  {
+    id: 2,
+    name: 'NIVEL AVANZADO TRABAJO SEGURO EN ALTURAS',
+    descripcion: '*REGIMEN CONTRIBUTIVO (Trabajador de Empresa)*. - Ficha de Inscripción Fotocopia de la CC al 150%. - Estar registrado en la plataforma - www.senasofiaplus.edu.co - Examen Médico Ocupacional (apto) para trabajo en alturas no mayor a un año (IPS avalada por la Secretaria de Salud Departamental). -Soporte de afiliación estado activo (ARL, EPS y PENSION) -Planilla de pago Seguridad Social en Salud, vigente a la fecha del curso (ARL, EPS y PENSION). -Certificado de la ARL del cumplimiento del Protocolo de Bioseguridad de la empresa. -Elementos de protección personal descritos en la ficha de inscripción.'
+  },
+  {
+    id: 3,
+    name: ' REENTRENAMIENTO NIVEL AVANZADO TRABAJO SEGURO EN ALTURAS',
+    descripcion: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
